3560580ab05a2cb51664a03eca2226bb1b4b8cd0
Yvonne Hong <yvonnehong2003@gmail.com>
2025-08-27 14:19:27 -0700

    test single resume file

--- tests/test_parser/test_pdf_extractor.py
+++ tests/test_parser/test_pdf_extractor.py
@@ -3,6 +3,17 @@ import sys
 sys.path.append('../../src') #go up two levels 
 from src.parser.pdf_extractor import PDFExtractor
 
-def test_pdf_extraction():
-    extractor = PDFExtractor()
+def test_single_resume_file(extractor, file_path, test_name):
+    print(f"\nTesting {test_name}")
+    result = extractor.extract_text_disk(file_path)
+    if result and result['extraction_status'] == 'success': 
+        print(f"Extraction is successful.")
+        print(f"File name: {result['file_name']}")
+        print(f"Total number of Pages: {result['page_count']}")
+        print(f"Total number of characters: {result['char_count']}")
+        print(f"First 150 chars: {result['full_text'][:150]}...")
+        return True 
+    else: 
+        print(f"The extraction failed.")
+        return False
 

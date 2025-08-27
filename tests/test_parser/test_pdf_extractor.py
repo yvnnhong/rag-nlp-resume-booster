bcd40e84ace51531648a34f4e032509c5df4e51f
Yvonne Hong <yvonnehong2003@gmail.com>
2025-08-26 17:01:16 -0700

    started test pdf extractor

--- tests/test_parser/test_pdf_extractor.py
+++ tests/test_parser/test_pdf_extractor.py
@@ -1,1 +1,8 @@
-#test pdf_extractor
+import logging
+import sys
+sys.path.append('../../src') #go up two levels 
+from src.parser.pdf_extractor import PDFExtractor
+
+def test_pdf_extraction():
+    extractor = PDFExtractor()
+
